7d86ed58047ea1006ac83f66dcbdc858a91884ef
Yevhen Moiseienko <mojseenko2011@gmail.com>
2022-04-15 11:49:20 +0300

    Added paginations data to CharList component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -11,29 +11,52 @@ class CharList extends Component{
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
     }
 
     componentDidMount() {
-        this.marvelService
-            .getAllCharacters()
-            .then(this.onCharLoaded)
-            .catch(this.onError)
+        this.onRequest();
     }
 
     marvelService = new MarvelService();
 
-    onCharLoaded = (charList) => {
-        this.setState({
-            charList,
-            loading: false
-        })
+    onCharListLoaded = (newCharList) => {
+
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true
+        }
+
+        this.setState(({charList, offset}) => ({
+            charList: [...charList, ...newCharList],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }))
     }
 
     onError = () => {
         this.setState({
             loading: false,
             error: true
+        })
+    }
+
+    onRequest = (offset) => {
+        this.onCharListLoading()
+        this.marvelService
+            .getAllCharacters(offset)
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
+
+    onCharListLoading = () => {
+        this.setState({
+            newItemLoading: true
         })
     }
 
@@ -44,8 +67,8 @@ class CharList extends Component{
                 {objectFit: 'contain'} :
                 {objectFit: 'cover'};
             return (
-                <li key={id}
-                    className="char__item"
+                <li className="char__item"
+                    key={id}
                     onClick={() => this.props.onCharSelected(id)}>
                     <img src={thumbnail} alt={name} style={objectFit}/>
                     <div className="char__name">{name}</div>
@@ -60,13 +83,17 @@ class CharList extends Component{
     }
 
     render() {
-        const {charList, loading, error} = this.state
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state
         const items = this.renderItems(charList)
         const content = loading? <Spinner/> : error? <Error/> : items
         return (
             <div className="char__list">
                 {content}
-                <button className="button button__main button__long">
+                <button
+                    className="button button__main button__long"
+                    style={{display: charEnded ? 'none' : 'block'}}
+                    disabled={newItemLoading}
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@ class MarvelService {
 
     _apiKey = 'apikey=46b7fa70a37536dbd1bc2d6e3cf4b36f';
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _baseOffset = 210;
 
     getResources  = async (url) => {
         const res = await fetch(url)
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transferCharacter)
     }
 

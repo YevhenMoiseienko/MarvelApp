04ba7c3a7d8c7a69c18640752c6e10f814c8addd
Yevhen Moiseienko <mojseenko2011@gmail.com>
2022-04-20 10:55:17 +0300

    Added routing

---
+++ src/components/pages/Comics.js
@@ -0,0 +1,14 @@
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
+
+
+const Comics = () => {
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList/>
+        </>
+    )
+}
+
+export default Comics;

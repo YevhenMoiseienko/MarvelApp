2a2465350b421b0f731cd8576f8fc524ae01f466
Yevhen Moiseienko <mojseenko2011@gmail.com>
2022-04-25 10:03:23 +0300

    Added different links and meta for all pages

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,3 +1,5 @@
+import {Helmet} from "react-helmet";
+
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
@@ -5,6 +7,13 @@ import ComicsList from "../comicsList/ComicsList";
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with our comics list"
+                />
+                <title>Comics page</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
